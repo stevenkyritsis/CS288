fa013d20b013550808b1b20ff75ff0b3f6825cf0
stevenkyritsis <16929888+stevenkyritsis@users.noreply.github.com>
2022-03-11 11:27:00 -0500

    up

--- homework_6/problem2.c
+++ homework_6/problem2.c
@@ -1,2 +1,33 @@
-//Problem2
+#include <stdio.h>
+#include <unistd.h>
 
+int main(int argc, char *argv[])
+{
+    int opt;
+    while((opt=getopt(argc,argv,"cduf:s:")) != -1) {
+        switch(opt) {
+            case 'c':
+            case 'd':
+            case 'u':
+                printf("Option: %c\n", opt);
+                break;
+            case 'f':
+                printf("Fields: %s\n", optarg);
+                break;
+            case 's':
+                printf("Char: %s\n", optarg);
+                break;
+            case ':' :
+                printf("Option needs a value\n");
+                break;
+            case '?':
+                printf("Unknown option: %c\n", optopt);
+                break;
+        }
+    }
+
+    for(; optind < argc; optind++)
+        printf("argument: %s\n", argv[optind]);
+    return 0;
+
+}
