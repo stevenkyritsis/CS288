c557ab9e9077c7ffa7540742c9538b9d53dad880
Steven Kyritsis <16929888+stevenkyritsis@users.noreply.github.com>
2022-04-16 16:57:38 -0400

    working on it

---
+++ homework_9/problem1.c
@@ -0,0 +1,16 @@
+//Problem 1
+
+#include<stdio.h>
+#include<stdlib.h>
+//#include<memory.h>
+
+int main(int argc, char *argv[]){
+
+    signed int input;
+
+    while(scanf("%d", &input) != EOF){
+        printf("Enter an integer: ");
+    }
+
+    return 0;
+}
