3325f0ff99e9911e0b78966745a0e3cd41c26607
stevenkyritsis <16929888+stevenkyritsis@users.noreply.github.com>
2022-06-08 09:08:33 -0400

    up

--- homework1/problem1.c
+++ homework1/problem1.c
@@ -1,10 +1,21 @@
-#include "stdio.h"
+#include <stdio.h>
 
 
 //Problem 1: CPU Cache Access Delay
 int main(){
 
+    const int SIZE = 20;
+    int array[SIZE];
     
+    for(int i=0; i < SIZE; i++){
+        array[i] = i;
+    }
+
+    int count = 0;
+    while(count < 1000){ //keeping array in cache
+        printf("%d \n",array[(count%SIZE)]); 
+        count++;
+    }
 
     return 0;
 }
