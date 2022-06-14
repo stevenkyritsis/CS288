54624459e431aa21b69311f3ee68b71ec608a471
Steven Kyritsis <16929888+stevenkyritsis@users.noreply.github.com>
2022-06-13 21:11:39 -0400

    problem 1 complete

--- homework1/problem1.c
+++ homework1/problem1.c
@@ -2,6 +2,7 @@
 //NJIT CS288-032 Professor Weiwei
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 
 
@@ -10,19 +11,26 @@ int main(){
 
     clock_t start, end;
     const int SIZE = 20;
-    int array[SIZE];
-    double access_time;
+    int *array=(int*)malloc(SIZE * sizeof(int));
+    double latency_access_time;
 
     //initialize the array
     for(int i=0; i < SIZE; i++){
         array[i] = i+1;
     }
 
-    int count = 0;
-    while(count < 1000){ //keeping array in cache
-        printf("%d \n",array[(count%SIZE)]); 
-        count++;
-    }
+    for(int j = 0; j < 4; j++){
+        start = clock();
+        int count = 0;
+        while(count < 1000){ //keeping array in cache
+            //printf("%d \n",array[(count%SIZE)]); used for testing purposes
+            count++;
+        }
+        end = clock();
 
+        latency_access_time = ((double) (end - start)) / CLOCKS_PER_SEC;
+        printf("The access time for CPU cache for loop %d: %f s\n",j,latency_access_time);
+    }
+    
     return 0;
 }
