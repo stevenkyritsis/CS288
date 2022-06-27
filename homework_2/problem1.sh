1c3676f58293a0a047d16e706af4bdad3609ac46
Steven Kyritsis <16929888+stevenkyritsis@users.noreply.github.com>
2022-06-27 11:37:08 -0400

    fixed the for loop issue

--- homework_2/problem1.sh
+++ homework_2/problem1.sh
@@ -1,21 +1,13 @@
 #!/bin/bash
 
-#AA
-wget https://en.wikipedia.org/wiki/AA -O /home/sak76/homework/homework_2/web/AA.html
-lynx -dump –nolist AA.html > grep -o [:alpha:] | /home/sak76/homework/homework_2/txt/AA.txt
-
-#AB
-wget https://en.wikipedia.org/wiki/AB -O /home/sak76/homework/homework_2/web/AB.html
-lynx -dump –nolist AB.html > /home/sak76/homework/homework_2/txt/AB.txt
-
-#AC
-wget https://en.wikipedia.org/wiki/AC -O /home/sak76/homework/homework_2/web/AC.html
-lynx -dump –nolist AC.html > /home/sak76/homework/homework_2/txt/AC.txt
-
-#ZY
-wget https://en.wikipedia.org/wiki/ZY -O /home/sak76/homework/homework_2/web/ZY.html
-lynx -dump –nolist ZY.html > /home/sak76/homework/homework_2/txt/ZY.txt
-
-#ZZ
-wget https://en.wikipedia.org/wiki/ZZ -O /home/sak76/homework/homework_2/web/ZZ.html
-lynx -dump –nolist ZZ.html > /home/sak76/homework/homework_2/txt/ZZ.txt
+for temp1 in {A..Z}
+do
+    for temp2 in {A..Z}
+    do
+        temp=$temp1$temp2
+        wget https://en.wikipedia.org/wiki/$temp -O /home/sak76/homework/homework_2/html/${temp}.html
+        lynx -dump –nolist /home/sak76/homework/homework_2/html/${temp}.html > /home/sak76/homework/homework_2/txt/${temp}.txt
+        grep -o [^a..zA..Z] /home/sak76/homework/homework_2/txt/${temp}.txt | sort | uniq
+        #echo $temp1$temp2
+    done
+done
