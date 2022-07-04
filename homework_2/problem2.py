22243283b139c671faf45f7adfad8a312f752f62
stevenkyritsis <16929888+stevenkyritsis@users.noreply.github.com>
2022-07-03 22:36:52 -0400

    Update problem2.py

--- homework_2/problem2.py
+++ homework_2/problem2.py
@@ -1,4 +1,32 @@
-import re
-import urllib
+from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import re
 
+letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+link = "https://en.wikipedia.org/wiki/"
+data = ''
+
+for l1 in letters:
+    for l2 in letters:
+        url=link+l1+l2
+        urlobject = urlopen(url)
+        contents = urlobject.read().decode("utf-8")
+        soup = BeautifulSoup(contents, "html.parser")
+        data += soup.get_text()
+    
+fdata = data.lower()
+
+key =  '[a-z]+'
+words = re.findall(key,fdata,flags=re.IGNORECASE)
+
+unique=[]
+for word in words:
+    if word not in unique:
+        unique.append(word)
+
+mydict = {}
+
+for word in unique:
+    mydict[word] = words.count(word)
+
+print(mydict)
