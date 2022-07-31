eaba0dea33c31c54b528d7009e9f8ca8d0e8665a
stevenkyritsis <16929888+stevenkyritsis@users.noreply.github.com>
2022-07-30 23:54:19 -0400

    Update problem1.c

---
+++ homework_4/problem1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int main(int argc, char *argv[]){
+    
+    int fd;
+    int *map;
+    
+    
+
+    return 0;
+}
